0c850018bc5ef87caac2e34a333c299518da4f8b
Nate M <64175378+nmccoard@users.noreply.github.com>
2020-10-03 20:16:46 -0400

    Added some more notes

--- quizNinja/main.js
+++ quizNinja/main.js
@@ -1,38 +1,87 @@
 //alert('Welcome to Quiz Ninja');
-document.getElementById("messageArea").innerHTML = "Click the button<br>to start the game";
-document.getElementById("myButton").addEventListener("click", quizTime);
+//document.getElementById("myButton").addEventListener("click", quizTime);
 
-function quizTime() {
-   const quiz = [
-      ["What is Superman's real name?", "Clark Kent"],
-      ["What is Wonder Woman's real name?", "Diana Prince"],
-      ["What is Batman's real name?", "Bruce Wayne"]
-   ];
+const view = {
+   score: document.querySelector('#score strong'),
+   question: document.getElementById('question'),
+   result: document.getElementById('result'),
+   info: document.getElementById('info'),
+   start: document.getElementById('start'),
+   render(target, content, attributes) {
+      for (const key in attributes) {
+         target.setAttribute(key, attributes[key]);
+      }
+      target.innerHTML = content;
+   },
+   show(element){
+      element.style.display = 'block';
+      },
+   hide(element){
+      element.style.display = 'none';
+      }
+};
 
-   let score = 0
-
-   // Game loop
-   for (const [question, answer] of quiz) {
-      const response = ask(question);
-      check(response, answer);
+const quiz = [{
+      name: "Superman",
+      realName: "Clark Kent"
+   },
+   {
+      name: "Wonder Woman",
+      realName: "Diana Prince"
+   },
+   {
+      name: "Batman",
+      realName: "Bruce Wayne"
+   },
+   {
+      name: "Aquaman",
+      realName: "Arthur Curry"
    }
-   // Call Game Over after asking all the questions
-   gameOver(score);
-}
+];
 
-   function ask(question) {
-      return prompt(question);
-   }
 
-   function check(response, answer) {
+const game = {
+   start(quiz) {
+      this.questions = [...quiz];
+      this.score = 0;
+      view.hide(view.start);
+
+      // Game loop
+      for (const question of this.questions) {
+         this.question = question;
+         this.ask();
+      }
+      // Call Game Over after asking all the questions
+      this.gameOver();
+   },
+
+   ask() {
+      const question = `What is ${this.question.name}'s real name?`;
+      view.render(view.question, question);
+      const response = prompt(question);
+      this.check(response);
+   },
+
+   check(response) {
+      const answer = this.question.realName;
       if (response === answer) {
-         alert('Correct!');
-         score++;
+         view.render(view.result, 'Correct!', {
+            'class': 'correct'
+         });
+         this.score++;
+         view.render(view.score, this.score);
       } else {
+         view.render(view.result, `Wrong! The correct answer was ${answer}`, {
+            'class': 'wrong'
+         });
          alert(`Wrong! The correct answer was ${answer}`);
       }
-   }
+   },
 
-   function gameOver(score) {
-      alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+   gameOver() {
+      view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+      view.show(view.start);
    }
+}
+
+view.start.addEventListener('click', () => game.start(quiz), false);
